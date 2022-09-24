5523dfae4df226d9b66a219a1cd37f0aa97d5a3c
Ana Beatriz <anabiawalker@gmail.com>
2022-09-24 10:07:23 -0300

    Adicionando projeto SpringBoot

---
+++ src/main/java/br/fatec/vidapet/VidapetApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.vidapet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidapetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidapetApplication.class, args);
+	}
+
+}
